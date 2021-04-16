6daff6e8239fe7d9f130030f2a1d29f5bd8dc07c
Claryyy <78910159+Claryyy@users.noreply.github.com>
2021-04-16 08:49:08 +0100

    Started on tasks 1 & 2

---
+++ main.js
@@ -0,0 +1,5 @@
+let moves = ["rock", "paper", "scissors"]
+
+let randomMove = moves[Math.floor(Math.random()*moves.length)];
+
+console.log(randomMove)
---
+++ task1.js
@@ -0,0 +1,42 @@
+
+let playerMove = "scissors";
+let computerMove = "paper";
+console.log(playerMove)
+console.log(computerMove)
+
+if ((playerMove === "rock") && (computerMove === "rock")) {
+    console.log("It's a draw")
+// return draw
+}
+else if ((playerMove === "rock") && (computerMove === "paper")) {
+    console.log("Computer wins")
+    // return computerWins
+}
+else if ((playerMove === "paper") && (computerMove === "rock")) {
+    console.log("Player wins")
+    // return playerWins
+}      
+else if ((playerMove === "paper") && (computerMove === "paper")) {
+    console.log("It's a draw")
+    // return draw
+}
+else if ((playerMove === "scissors") && (computerMove === "paper")) {
+    console.log("Player wins")
+    // return playerWins
+}
+else if ((playerMove === "paper") && (computerMove === "scissors")) {
+    console.log("Computer wins")
+    // return computerWins
+}       
+else if ((playerMove === "scissors") && (computerMove === "scissors")) {
+    console.log("It's a draw")
+    // return draw
+}
+else if ((playerMove === "scissors") && (computerMove === "rock")) {
+    console.log("Computer wins")
+    // return computerWins
+}
+else if ((playerMove === "rock") && (computerMove === "scissors")) {
+    console.log("Player wins")
+    // return playerWins
+}  
