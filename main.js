a8b2d9467b88c59f5545b4b9fc7f9ae4e7b7eb50
Claryyy <78910159+Claryyy@users.noreply.github.com>
2021-04-27 12:22:14 +0200

    Started on user input to add username. But need to make sure player can have a chance to input a valid username again

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 // DOM variables
+const username = document.querySelector("username");
+const submitButton = document.querySelector(".submitButton");
 const startButton = document.querySelector(".startGameButton");
 const roundsPlayed = document.querySelector(".roundsPlayed");
 const wins = document.querySelector(".wins");
@@ -10,12 +12,62 @@ const scissorsButton = document.querySelector(".scissorsButton");
 const thisRoundResult = document.querySelector(".thisRoundResult");
 const keepPlaying = document.querySelector(".keepPlaying");
 const stopPlaying = document.querySelector(".stopPlaying");
+const game = document.querySelector(".game");
+// const encore = document.querySelector(".encore");
 
 //Game variables 
 let gameRoundsPlayed = 0;
 let gameWins = 0;
 let gameLosses = 0;
 let gameDraws = 0;
+let gameUsername = "";
+let computerMove = "";
+let newComputerMove = "";
+
+game.style.visibility = "hidden";
+// encore.style.visibility = "hidden";
+
+function usernameForm () {
+    let usernameInput = document.getElementById("username-input");
+    let username = usernameInput.value;
+  
+    if (username.length > 10) {
+      alert("Your username must be 10 or fewer characters");
+username = usernameInput.value;
+    } else if (!username[0].match(/[a-zA-Z]/)) {
+      alert("Your username must begin with a letter. Numbers or special characters are not allowed.");
+
+    } else if (username[0].match(/[a-z]/)) {
+      alert("Your username must begin with an uppercase letter");
+
+    } else {
+      gameUsername = username;
+    }
+  
+    let form = document.getElementById("form");
+    form.style.display = "none";
+  
+    username.innerText = `Player: ${gameUsername}`;
+    username.style.display = "block";
+  }
+
+submitButton.addEventListener("click", usernameForm);
+
+  // Add enter keydown listener
+document.getElementById('username-input').addEventListener(
+    'keydown',
+    (event) => {
+      if (event.defaultPrevented) {
+        return;
+      }
+      if (event.key === 'Enter') {
+        console.log('enter event listener');
+        formSubmit();
+      }
+      event.preventDefault;
+    },
+    false
+  );
 
 
 function getRandomMove() {
@@ -26,6 +78,8 @@ function getRandomMove() {
 function startGame() {
     startButton.style.visibility = "hidden";
 computerMove = getRandomMove();
+game.style.visibility = "visible";
+// encore.style.visibility = "hidden";
 }
 
 startButton.addEventListener("click", startGame)
@@ -48,6 +102,7 @@ if (computerMove === "scissors") {
 }
 gameRoundsPlayed ++;
 roundsPlayed.innerText = `Rounds played: ${gameRoundsPlayed}`;
+// encore.style.visibility = "visible";
 }
 
 rockButton.addEventListener("click", checkRock)
@@ -70,6 +125,7 @@ if (computerMove === "rock") {
 }
 gameRoundsPlayed ++;
 roundsPlayed.innerText = `Rounds played: ${gameRoundsPlayed}`;
+// encore.style.visibility = "visible";
 }
 
 paperButton.addEventListener("click", checkPaper)
@@ -92,18 +148,23 @@ if (computerMove === "paper") {
 }
 gameRoundsPlayed ++;
 roundsPlayed.innerText = `Rounds played: ${gameRoundsPlayed}`;
+// encore.style.visibility = "visible";
 }
 
 scissorsButton.addEventListener("click", checkScissors)
 
 function continueToPlay() {
-    computerMove = getRandomMove();
+    newComputerMove = getRandomMove();
+    computerMove = newComputerMove;
+    // encore.style.visibility = "hidden";
 }
 
 keepPlaying.addEventListener("click", continueToPlay)
 
-// function endGame(){
-// startButton.style.visibility = "initial";
-// }
+function endGame() {
+startButton.style.visibility = "visible";
+game.style.visibility = "hidden";
+// encore.style.visibility = "hidden";
+}
 
-// stopPlaying.addEventListener("click", endGame)
+stopPlaying.addEventListener("click", endGame)
